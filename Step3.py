33c3c182e2c54172616a1238eedb15a2c42324f9
Thalia <thaliavillalobos@student.hartnell.edu>
2016-11-01 20:41:27 -0700

    For program 3, I created a program that finds a word from a list and returns the index. In order to do so, I had to split the dictionry needle(the target) and haystack(the list of value).

---
+++ Step3.py
@@ -0,0 +1,33 @@
+import urllib2
+import requests
+import json
+
+def main():
+    #requsting information from API
+    payload = {'token':'', 'needle':'', 'haystack': ''}
+    data= requests.post("http://challenge.code2040.org/api/haystack", params = payload)
+
+    #To view the information
+    #print(data.text)
+
+    #Coverting JSON string into Python dictionary
+    dictionary = json.loads(data.text)
+
+    #Split the dictionay into needle(target) and haystack(list of value)
+    needle = dictionary["needle"]
+    haystack = dictionary["haystack"]
+
+    #Getting the index where the needle is located
+    index = haystack.index(needle)
+
+    #Checking needle, haystack, and index 
+    #print(needle)
+    #print(haystack)
+    #print(index)
+
+    #Posting my results
+    load = {'token':'', 'needle':'', 'haystack': ''}
+    requests.post("http://challenge.code2040.org/api/haystack/validate", params =load)
+    
+
+main()
